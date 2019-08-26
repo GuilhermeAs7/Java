0f08a3bc7dc1ff0df53ab540aa5a09e6e23e20be
Guilherme Alexandre <32078978+GuilhermeAs7@users.noreply.github.com>
2019-08-26 19:48:17 -0300

    Update Exercicio_AlunoTeste.java

--- Exercicio_AlunoTeste.java
+++ Exercicio_AlunoTeste.java
@@ -3,7 +3,7 @@ public class AlunoTeste {
 
 	public static void main(String[] args) {
 		
-		Aluno aluno = new Aluno(); 
+		Aluno aluno = new Aluno(); // Criando classe, utilizando aluno como variavel.
 		
 		aluno.setNome("Wesley");
 		aluno.setMatricula("0716259");
